8bbda4c61af70b97c0c9d8072e644085b3f2da4c
jaydunb12 <74875595+jaydunb12@users.noreply.github.com>
2023-03-01 13:25:47 -0500

    update

--- front/src/pages/register/Register.js
+++ front/src/pages/register/Register.js
@@ -7,10 +7,12 @@ const Register = () => {
         <div className='container'>
             <div className='register-box'>
             <h2 className='register-text'>Ready to Join?</h2>
+            <form onSubmit={}>
             <input className='register-input' type="email" placeholder="Email" required />
             <input className='register-input' type="text" placeholder="Password" required />
             <input className='register-input' type="password" placeholder="Username" required />
             <input className='register-submit' type="submit" value="Login"></input>
+            </form>
             </div>
         </div>
     </main>
